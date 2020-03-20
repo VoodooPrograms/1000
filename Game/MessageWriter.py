0641aae9476012e36e38cfa3429eb98652389dc5
VoodooPrograms <arkanidius@gmail.com>
2020-03-20 16:12:52 +0100

    Separate MessageWriter into his own class

---
+++ Game/MessageWriter.py
@@ -0,0 +1,24 @@
+import json
+
+from Game.JsonEncoder import JsonEncoder
+
+
+class MessageWriter:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def sendMessage(self, message):
+        print(self.socket.connections)
+        self.socket.write_message(json.dumps(message, cls=JsonEncoder))
+
+    def emitMessage(self, message):
+        [client.write_message(json.dumps(message, cls=JsonEncoder)) for client in self.socket.connections]
+
+    def sendError(self, exception):
+        jsonResponse = {}
+        jsonResponse['response'] = 'exception'
+        jsonResponse['resultMessage'] = str(exception)
+        self.socket.write_message(jsonResponse, cls=JsonEncoder)
+
+    def closeConnection(self):
+        self.socket.close()
